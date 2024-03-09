36548f2088a6f60d3e3392f6f8f2c902a8e84cab
Adithya S K <adithyaskolavi@gmail.com>
2024-03-09 23:44:39 +0530

    updated download dataset functionality - Adithya S K

---
+++ trainer/download.sh
@@ -0,0 +1,3 @@
+python download_dataset.py \
+    --connection_string "DefaultEndpointsProtocol=https;AccountName=<Account Name>;AccountKey=<Account Key>;EndpointSuffix=core.windows.net" \
+    --local_folder_path "/path/to/local/folder"
